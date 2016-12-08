c5f1337964fc43c44a304e4ca81a8e9b3fe22010
copenbacon <sclary50@gmail.com>
2016-12-08 09:19:27 -0800

    refactoring

--- src/series.py
+++ src/series.py
@@ -15,7 +15,7 @@ def fibonacci(n):
 def lucas(n):
     """Lucas sequence."""
     if n < 0:
-        return None
+        print('Not a valid number!')
     if n == 0:
         return 2
     elif n == 1:
@@ -28,7 +28,7 @@ def lucas(n):
 def sum_series(n, k=0, l=1):
     """Sum Series Sequence with optional parameters k and l."""
     if n < 0:
-        return None
+        print('Not a valid number!')
     elif n == 0:
         return k
     elif n == 1:
@@ -39,11 +39,8 @@ def sum_series(n, k=0, l=1):
 
 if __name__ == "__main__":
     print(fibonacci.__doc__)
-    print('fibonacci(2) = ')
-    print(fibonacci(2))
+    print('fibonacci(2) = ', fibonacci(2))
     print(lucas.__doc__)
-    print('lucas(2) = ')
-    print(lucas(2))
+    print('lucas(2) = ', lucas(2))
     print(sum_series.__doc__)
-    print('sum_series(2) = ')
-    print(sum_series(2))
+    print('sum_series(2) = ', sum_series(2))
